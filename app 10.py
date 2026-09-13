52f37350b50915d320c5c5489b634722f34f4362
wolf346 <aldomontoya09@gmail.com>
2026-09-13 08:18:26 -0300

    Update app 10.py

--- app 10.py
+++ app 10.py
@@ -5,15 +5,15 @@ import string
 from datetime import datetime, timedelta, timezone
 from zoneinfo import ZoneInfo
 import streamlit as st
-# Conexión automática a Supabase (PostgreSQL)
 
-conn = st.connection("postgresql", type="sql")
 # Configuración general de la página
 st.set_page_config(
     page_title="Evaluaciones Psicológicas Forenses",
     page_icon="⚖️",
     layout="centered",
 )
+# Conexión automática a Supabase (PostgreSQL)
+conn = st.connection("postgresql", type="sql")
 
 # -----------------------------------------------------------------------------
 # OCULTAR MENÚ, FOOTER Y CABECERA DE STREAMLIT (GITHUB / SHARE)
